fe6db1e22f6929da3250fa28bb33c9459379144a
Jane <janni8bird@gmail.com>
2021-10-24 19:05:25 +0300

    2 and 3 exs added

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ window.onload = function() {
     const testInput1 = document.querySelector('.test1-input');
     const testSubmit1 = document.querySelector('.test1-submit');
     const testContainer1 = document.querySelector('.test1-container');
+    const testInput2 = document.querySelector('.test2-input');
+    const testSubmit2 = document.querySelector('.test2-submit');
+    const testContainer2 = document.querySelector('.test2-container');
+    const testContainer3 = document.querySelector('.test3-container');
+    const testGenerateArray = document.querySelector('.test3-generate');
     testSubmit1.addEventListener('click', (event) => {
         const result = testInput1.value;
         const resultElement = document.createElement('p');
@@ -11,6 +16,36 @@ window.onload = function() {
         	resultElement.appendChild(msg);
         	testContainer1.appendChild(resultElement);
         }
-    })
+    });
+    testSubmit2.addEventListener('click', (event) => {
+    	const result = testInput2.value;
+    	const resultElement = document.createElement('p');
+    	let msg = null;
+    	if(result === 'Вячеслав') {
+        	msg = document.createTextNode('Привет, Вячеслав');
+        } else {
+        	msg = document.createTextNode('Нет такого имени');
+        }
+        resultElement.appendChild(msg);
+        testContainer2.appendChild(resultElement);
+    });
+    testGenerateArray.addEventListener('click', (event) => {
+    	const generateRandomNubmer = function() {
+    		return Math.floor(Math.random() * (100 - 1) + 1);
+    	};
+    	const randomArray = [generateRandomNubmer(), generateRandomNubmer(), generateRandomNubmer(), generateRandomNubmer()];
+    	console.log(randomArray.toString());
+    	const findDivByThree = function(arr) {
+    		const results = arr.filter(element => element % 3 === 0);
+    		if(!results.length) {
+    			return '-';
+    		}
+    		return results.toString();
+    	};
+    	const resultElement = document.createElement('p');
+    	const randomArrayString = document.createTextNode('[' + randomArray.toString() + '] => ' + findDivByThree(randomArray));
+    	resultElement.appendChild(randomArrayString);
+    	testContainer3.appendChild(resultElement);
+    });
 };
 
